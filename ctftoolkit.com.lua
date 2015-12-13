b7f931d0b10910c7177ea71c60ec4ca4ce54832b
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-12-13 16:43:05 -0600

    Add Email

--- ctftoolkit.com.lua
+++ ctftoolkit.com.lua
@@ -2,3 +2,9 @@ a(_a, "204.232.175.78")
 cname("www", "ID10T-Errors.github.io")
 
 a("internal", "107.189.33.181")
+
+mx(_a, "eforward1.registrar-servers.com", 10)
+mx(_a, "eforward2.registrar-servers.com", 10)
+mx(_a, "eforward3.registrar-servers.com", 10)
+mx(_a, "eforward4.registrar-servers.com", 15)
+mx(_a, "eforward5.registrar-servers.com", 20)
