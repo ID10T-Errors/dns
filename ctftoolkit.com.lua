69ce21bd6c6b0ad639e95dffe795272ba1686492
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2016-01-19 09:32:08 -0600

    Add Minecraft server

--- ctftoolkit.com.lua
+++ ctftoolkit.com.lua
@@ -3,6 +3,9 @@ a(_a, "192.30.252.154")
 cname("www", "ID10T-Errors.github.io")
 cname("workshop", "ID10T-Errors.github.io") 
 
+a("minecraft", "158.69.226.20")               -- Games
+srv("minecraft", "minecraft.ctftoolkit.com", 25601, 5, 5)
+
 a("internal", "107.189.33.181")               -- GitLab, etc.
 
 mx(_a, "mx1.improvmx.com", 10)                -- Email forwarding
