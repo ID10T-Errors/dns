dcbd89285fc3584d95cd8d59cf0bf7b960363052
Zeb McCorkle <zebmccorkle@gmail.com>
2016-06-13 13:32:38 -0400

    Update ctftoolkit.com.lua

--- ctftoolkit.com.lua
+++ ctftoolkit.com.lua
@@ -6,6 +6,19 @@ cname("workshop", "ID10T-Errors.github.io")
 a("minecraft", "158.69.226.20")               -- Games
 srv("_minecraft._tcp.minecraft.ctftoolkit.com", "minecraft.ctftoolkit.com", 25601, 5, 5)
 
+a("blog", "52.0.16.118")                      -- Medium Blog
+a("blog", "52.1.119.170")
+a("blog", "52.1.147.205")
+a("blog", "52.1.173.203")
+a("blog", "52.4.145.119")
+a("blog", "52.4.175.111")
+a("blog", "52.4.225.124")
+a("blog", "52.4.240.221")
+a("blog", "52.4.38.70")
+a("blog", "52.5.181.79")
+a("blog", "52.6.3.192")
+a("blog", "52.6.46.142")
+
 --srv(name, target, port, prio, weight, ttl)
 
 a("services", "104.236.80.60")                -- GitLab, etc.
