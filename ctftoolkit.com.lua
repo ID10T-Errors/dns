894e97ccb13c395da1f95e507117ec8961c4abc5
Zeb McCorkle <zebmccorkle@gmail.com>
2016-06-13 13:37:30 -0400

    Update ctftoolkit.com.lua

--- ctftoolkit.com.lua
+++ ctftoolkit.com.lua
@@ -1,7 +1,7 @@
 a(_a, "192.30.252.153")                       -- GitHub Pages
 a(_a, "192.30.252.154")
-cname("www", "ID10T-Errors.github.io")
-cname("workshop", "ID10T-Errors.github.io") 
+cname("www", "SignumCollective.github.io")
+cname("workshop", "SignumCollective.github.io") 
 
 a("minecraft", "158.69.226.20")               -- Games
 srv("_minecraft._tcp.minecraft.ctftoolkit.com", "minecraft.ctftoolkit.com", 25601, 5, 5)
