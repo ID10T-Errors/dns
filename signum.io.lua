5876ffb49f6e2094928c2fc75c19aa41cd9b28be
Zeb McCorkle <zebmccorkle@gmail.com>
2016-06-13 15:28:47 -0400

    Update and rename ctftoolkit.com.lua to signum.io.lua

--- signum.io.lua
+++ signum.io.lua
@@ -4,7 +4,7 @@ cname("www", "SignumCollective.github.io")
 cname("workshop", "SignumCollective.github.io") 
 
 a("minecraft", "158.69.226.20")               -- Games
-srv("_minecraft._tcp.minecraft.ctftoolkit.com", "minecraft.ctftoolkit.com", 25601, 5, 5)
+srv("_minecraft._tcp.minecraft.signum.io", "minecraft.signum.io", 25601, 5, 5)
 
 a("blog", "52.0.16.118")                      -- Medium Blog
 a("blog", "52.1.119.170")
@@ -24,7 +24,7 @@ cname("2d756d622e76035bf8923c4eeb96153e.blog", "b1a41190c95e63c66e7026d49c4a8e55
 
 a("services", "104.236.80.60")                -- GitLab, etc.
 aaaa("services", "2604:a880:800:10::701:8001")
-cname("*.services", "services.ctftoolkit.com")
+cname("*.services", "services.signum.io")
 
 mx(_a, "mx1.improvmx.com", 10)                -- Email forwarding
 mx(_a, "mx2.improvmx.com", 20)
